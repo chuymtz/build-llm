4306e650005313fae17dbc3e600f1c6513b3fce3
JosedeJesus_Martinez <josedejesus_martinez@oxy.com>
2025-08-08 18:20:50 -0500

    test:

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import torch.nn as nn
 import json
 import os, pprint
 import matplotlib.pyplot as plt
-
+import tiktoken
+tokenizer = tiktoken.get_encoding("gpt2")
 os.getcwd()
 
 with open("config.json", "r") as f:
@@ -105,54 +106,138 @@ class CausalAttention(nn.Module):
         context_vec = attn_weights @ values
         return context_vec
         
-
 class MultiHeadAttention(nn.Module):
     def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
         super().__init__()
-        self.heads = nn.ModuleList([
-            CausalAttention(
-                d_in, d_out, context_length, dropout, qkv_bias
-            )
-            for _ in range(num_heads)
-        ])
+        assert d_out % num_heads == 0, "d_out must be divisible by num_heads"
+
+        self.d_out = d_out
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads # Reduce the projection dim to match desired output dim
+
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.out_proj = nn.Linear(d_out, d_out)  # Linear layer to combine head outputs
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer('mask', torch.triu(torch.ones(context_length, context_length), diagonal=1))
+
     def forward(self, x):
-        return torch.cat([head(x) for head in self.heads], dim=-1)
+        b, num_tokens, d_in = x.shape
+
+        keys = self.W_key(x) # Shape: (b, num_tokens, d_out)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        # We implicitly split the matrix by adding a `num_heads` dimension
+        # Unroll last dim: (b, num_tokens, d_out) -> (b, num_tokens, num_heads, head_dim)
+        keys = keys.view(b, num_tokens, self.num_heads, self.head_dim) 
+        values = values.view(b, num_tokens, self.num_heads, self.head_dim)
+        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+
+        # Transpose: (b, num_tokens, num_heads, head_dim) -> (b, num_heads, num_tokens, head_dim)
+        keys = keys.transpose(1, 2)
+        queries = queries.transpose(1, 2)
+        values = values.transpose(1, 2)
+
+        # Compute scaled dot-product attention (aka self-attention) with a causal mask
+        attn_scores = queries @ keys.transpose(2, 3)  # Dot product for each head
+        
+        # Original mask truncated to the number of tokens and converted to boolean
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
+
+        # Use the mask to fill attention scores
+        attn_scores.masked_fill_(mask_bool, -torch.inf)
+        
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+
+        # Shape: (b, num_tokens, num_heads, head_dim)
+        context_vec = (attn_weights @ values).transpose(1, 2) 
+        
+        # Combine heads, where self.d_out = self.num_heads * self.head_dim
+        context_vec = context_vec.contiguous().view(b, num_tokens, self.d_out)
+        context_vec = self.out_proj(context_vec) # optional projection
+
+        return context_vec
 
 class TransformerBlock(nn.Module):
     def __init__(self, cfg):
         super().__init__()
-        self.cfg = cfg
+        self.att = MultiHeadAttention(
+            d_in=cfg["emb_dim"],
+            d_out=cfg["emb_dim"],
+            context_length=cfg["context_length"],
+            num_heads=cfg["n_heads"], 
+            dropout=cfg["drop_rate"],
+            qkv_bias=cfg["qkv_bias"])
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg["emb_dim"])
+        self.norm2 = LayerNorm(cfg["emb_dim"])
+        self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
 
+    def forward(self, x):
+        # Shortcut connection for attention block
+        # y = x
+        # x = y
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)  # Shape [batch_size, num_tokens, emb_size]
+        x = self.drop_shortcut(x)
+        x = x + shortcut  # Add the original input back
 
+        # Shortcut connection for feed forward block
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut  # Add the original input back
 
-class DummyGPTModel(nn.Module):
+        return x
+
+class GPTModel(nn.Module):
     def __init__(self, cfg):
         super().__init__()
-        self.cfg = cfg
-        self.tok_emb = nn.Embedding(self.cfg['vocab_size'], self.cfg['emb_dim'])
-        self.pos_emb = nn.Embedding(self.cfg['context_length'], self.cfg['emb_dim'])
-        self.drop_emb = nn.Dropout(self.cfg['drop_rate'])
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
         
         self.trf_blocks = nn.Sequential(
-            *[TransformerBlock(self.cfg) for _ in range(self.cfg['n_layers'])]
-        )
+            *[TransformerBlock(cfg) for _ in range(cfg["n_layers"])])
         
-        self.final_norm = LayerNorm(self.cfg['emb_dim'])
-        
+        self.final_norm = LayerNorm(cfg["emb_dim"])
         self.out_head = nn.Linear(
-            self.cfg['emb_dim'], self.cfg['vocab_size'], bias=False
+            cfg["emb_dim"], cfg["vocab_size"], bias=False
         )
-    
-    def forward(self, x):
-        """Forward pass of the GPT model.
-        Args:
-            x (torch.Tensor): Input tensor of shape (batch_size, seq_len).
-        Returns:
-            torch.Tensor: Output tensor of shape (batch_size, seq_len, vocab_size).
-        """
-        return x
-        
-gptmodel = self = DummyGPTModel(GPT_CONFIG_124M)
+
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape
+        tok_embeds = self.tok_emb(in_idx)
+        pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
+
+        x = tok_embeds + pos_embeds  # Shape [batch_size, num_tokens, emb_size]
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
+
+torch.manual_seed(123)
+model = self = GPTModel(cfg=GPT_CONFIG_124M)
+
+batch = []
+txt1 = "Every effort moves you"
+txt2 = "Every day holds a"
+batch.append(torch.tensor(tokenizer.encode(txt1)))
+batch.append(torch.tensor(tokenizer.encode(txt2)))
+batch = torch.stack(batch, dim=0)
+print(batch.shape)
+
+out = model(batch)
+
+print("Input batch:\n", batch)
+print("\nOutput shape:", out.shape)
+print(out)
 
 
 dummy_input = torch.tensor([[1, 5, 2], [4, 3, 0]])  # shape: (batch_size=2, seq_len=3)
